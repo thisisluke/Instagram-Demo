568eeb8eef218ee6230f86fd812c0eabcc9e4082
thisisluke <luke.liu.au@outlook.com>
2020-09-11 23:49:18 +1000

    New onboarding and general viewControllers

---
+++ Instagram/Instagram/Controllers/Onboarding/RegistrationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationViewController.swift
+//  Instagram
+//
+//  Created by Liu Changhong on 11/9/20.
+//  Copyright © 2020 Luke Liu. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
