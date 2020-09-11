9b2de107c39a5dc97dd11911e5780ab52666e47c
thisisluke <luke.liu.au@outlook.com>
2020-09-11 14:15:11 +1000

    Project Init

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Liu Changhong on 11/9/20.
+//  Copyright © 2020 Luke Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
